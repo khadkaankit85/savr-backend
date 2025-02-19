7a7f1d953b59fe2c06c236067b17be5d04fc1e15
Mattazz <matthewazada101@gmail.com>
2025-02-19 14:20:26 -0500

    add Giant Tiger scraping function with error handling and data transformation

--- src/scrapes/bestbuy.ts
+++ src/scrapes/bestbuy.ts
@@ -28,7 +28,7 @@ export async function scrapeBestbuy(keyword: string): Promise<{ name: string; re
         const data = response.data;
 
         console.log(data);
-        
+
 
         // Map and transform the product data
         return data.products.map((product) => ({
@@ -43,13 +43,66 @@ export async function scrapeBestbuy(keyword: string): Promise<{ name: string; re
     }
 }
 
+
+
 // "x-algolia-agent": "Algolia%20for%20JavaScript%20(4.15.0)%3B%20Browser%20(lite)",
 // "X-Algolia-API-Key": "1ec86e7ee6661988fb72e0c843badcd8",  # Replace with your API key
 // "X-Algolia-Application-Id": "NGMHTYXT0T",  # Replace with your Application ID
 
+interface GiantTigerProduct {
+    title: string;
+    price: number;
+}
 
-export async function scrapeGiantTiger(){
+interface GiantTigerResponse {
+    results: {
+        hits: GiantTigerProduct[];
+    }[];
+}
+
+export async function scrapeGiantTiger() {
     const URL = "https://ngmhtyxt0t-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.15.0)%3B%20Browser%20(lite)&x-algolia-api-key=1ec86e7ee6661988fb72e0c843badcd8&x-algolia-application-id=NGMHTYXT0T"
-    
+    const search_query = "table"
+
+    const headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": random_user_agent(),
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+    }
+
+    let payload = {
+        "requests": [
+            {
+                "indexName": "shopify_products",
+                "hitsPerPage": "1000",
+                "distinct": "false",
+                "analytics": "false",
+                "clickAnalytics": "false",
+                "page": 0,
+                "params": `query=${search_query}&hitsPerPage=100&facets=%5B%22product_type%22%5D&tagFilters=`,
+            }
+        ]
+    }
+
+    try {
+        const response = await axios.post<GiantTigerResponse>(URL, payload, { headers });
+        const data = response.data;
+
+        // Parse and transform the product data
+        const parsedData = data.results[0].hits.map((product: any) => ({
+            title: product.title,
+            price: product.price
+        }));
+
+        console.log(parsedData);
+
+        return parsedData;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw new Error('Failed to scrape data'); // Ensure proper error propagation
+    }
+
+
 
 }
