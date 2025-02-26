0f4bc58c3c3382c642d195d2e61ccf36a93f9b4f
Mattazz <matthewazada101@gmail.com>
2025-02-26 14:57:16 -0500

    Added incomplete scrapes for sportCheck and Simons

--- src/scrapes/scraper.ts
+++ src/scrapes/scraper.ts
@@ -92,6 +92,75 @@ interface GiantTigerResponse {
   }[];
 }
 
+interface SimonsProductResponse{
+  results: {
+    hits: SimonsProduct[];
+  }[];
+}
+interface SimonsProduct {
+  title?: string;
+  brand_name?: string;
+  regular_price?: number;
+  price: number;
+  image: string;
+}
+
+export async function scrapeSimons(keyword: string,
+): Promise<{ title?: string; brand_name?: string; price: number;image: string;}[]> {
+  const searchString = keyword;
+  const algoliaAPIKey = "cafaab73d1b1e8b2721d108fc92ddc99"
+  const algoliaID = "7BMC2DATKE"
+
+  const URL = `https://7bmc2datke-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.17.0)%3B%20Browser%20(lite)%3B%20instantsearch.js%20(4.55.0)%3B%20Vue%20(2.7.14)%3B%20Vue%20InstantSearch%20(4.9.0)%3B%20JS%20Helper%20(3.12.0)&x-algolia-api-key=${algoliaAPIKey}&x-algolia-application-id=${algoliaID}`
+  const BASE_URL_IMG = "https://imagescdn.simons.ca/"
+
+  const headers = {
+    "Content-Type": "application/json",
+    "User-Agent": random_user_agent(),
+    Accept: "*/*",
+    Connection: "keep-alive",
+  }
+
+  const payload = {
+    requests: [
+      {
+        indexName: "simons_products_en-CA",
+        hitsPerPage: 1000,
+        distinct: false,
+        analytics: false,
+        clickAnalytics: false,
+        page: 0,
+        params: `query=${encodeURIComponent(searchString)}&hitsPerPage=100&facets=["product_type"]&tagFilters=`
+      }
+    ]
+  }
+
+  try {
+    const response = await axios.post<SimonsProductResponse>(URL, payload, {
+      headers,
+    });
+    const data = response.data;
+    // console.log(data);
+
+    // Parse and transform the product data
+    const parsedData = data.results[0].hits.map(
+      (product: SimonsProduct) => ({
+        title: product.title || product.brand_name,
+        price: product.price,
+        image: `${BASE_URL_IMG}${product.image}`
+      }),
+    );
+
+    //  console.log(parsedData);
+
+    return parsedData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw new Error("Failed to scrape data");
+  }
+
+}
+
 export async function scrapeGiantTiger(
   keyword: string,
 ): Promise<{ title: string; price: number; image: string; url: string }[]> {
@@ -190,3 +259,58 @@ export async function scrapeCadTire(keyword: string) {
     throw new Error(`Failed to scrape data`);
   }
 }
+
+
+// NOT WORKING NOT SURE WHY IT'S THE SAME ARCHITECTURE AS CADTIRE
+export async function scrapeSportCheck(keyword: string) {
+  const searchString = keyword;
+  const sportCheckSubKey: string = "c01ef3612328420c9f5cd9277e815a0e";
+  const storeCode: string = "5141";
+
+  const URL: string = `https://apim.sportchek.ca/v1/search/v2/search?q=${searchString}&store=${storeCode}`;
+
+  const headers = {
+    "authority": "apim.sportchek.ca",
+    "Content-Type": "application/json",
+    "User-Agent": random_user_agent(),
+    "Accept": "*/*",
+    "Connection": "keep-alive",
+    "Referer": "https://www.sportchek.ca/",
+    "Dnt": "1",
+    "ocp-apim-subscription-key": `${sportCheckSubKey}`,
+  };
+
+  try {
+    const response = await axios.get<CadTireResponse>(URL, { headers });
+    const data = response.data;
+
+    console.log(data);
+
+
+    // log(data.products)
+    if (!data.products || data.products.length === 0) {
+      // logic for brand search
+      // log(data)
+    } else {
+      const parsedData = data.products.map((product: CadTireProduct) => ({
+        title: product.title,
+        url: product.url,
+        currentPrice: product.currentPrice.value,
+        image: product.images[0].url,
+      }));
+
+      log("this is the parsed data")
+      console.log(parsedData);
+      return parsedData;
+    }
+  } catch (error) {
+    console.log(`Error fetching data: `, error);
+    throw new Error(`Failed to scrape data`);
+  }
+}
+
+
+// Call the function to test it
+scrapeSimons("shirt")
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
