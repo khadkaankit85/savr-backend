c5043a2ef96bcb6fb2080835e75a6ccaafb73549
Angkit Khadka <khadkaankit85@gmail.com>
2025-03-04 13:27:33 -0500

    route to get url from frontend, track page

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import productModel from "../schema/productSchema";
+import { log } from "console";
 const router = express.Router();
 
 //<domain>/<routername>/autocompletion?query=<productname>&limit=<limit>
@@ -9,6 +10,21 @@ router.get("/autocompletion", async (req: Request, res: Response) => {
     .find({ name: { $regex: query, $options: "i" } })
     .limit(10);
   res.json({ suggestions });
+});
+
+router.post("/track-product", async (req: Request, res: Response) => {
+  const { url } = req.body;
+  log(url);
+
+  if (!url) {
+    res
+      .status(400)
+      .json({ success: false, message: "Product URL is required." });
+    return;
+  }
+
+  try {
+  } catch (error) {}
 });
 
 export default router;
