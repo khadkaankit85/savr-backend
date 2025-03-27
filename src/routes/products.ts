a93a5f1e7c74fa82b152269ece143bd3da097598
Angkit Khadka <khadkaankit85@gmail.com>
2025-03-26 21:09:40 -0400

    product detail chart page

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import productModel from "../schema/productSchema";
 import { log } from "console";
+import bestBuy_products from "../models/bestBuyData";
 const router = express.Router();
 
 //<domain>/<routername>/autocompletion?query=<productname>&limit=<limit>
@@ -24,7 +25,25 @@ router.post("/track-product", async (req: Request, res: Response) => {
   }
 
   try {
-  } catch (error) {}
+  } catch (error) { }
 });
+
+router.get("/history", async (req: Request, res: Response) => {
+  console.log(req.query)
+  const productId = req.query.productId
+  if (!productId) {
+    res.status(404).json({ message: "not found" })
+    return;
+  }
+  try {
+    const product = await bestBuy_products.findOne({
+      _id: productId
+    })
+    res.json({ product })
+  }
+  catch {
+    res.status(500).json({ message: "internal server error while querying the database for asked product" })
+  }
+})
 
 export default router;
