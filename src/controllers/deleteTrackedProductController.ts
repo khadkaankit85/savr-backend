4502e18425915285e0936585d9ac881fb0cee9c9
Mattazz <matthewazada101@gmail.com>
2025-04-04 11:55:17 -0400

    modify the delete route

--- src/controllers/deleteTrackedProductController.ts
+++ src/controllers/deleteTrackedProductController.ts
@@ -1,32 +1,34 @@
-import { Request, Response } from 'express';
-import mongoose from 'mongoose';
+import { Request, Response } from "express";
+import mongoose from "mongoose";
 import User from "../schema/userSchema";
 
 interface DeleteTrackedProductRequest extends Request {
-    body: {
-        userId: string;
-        productId: string;
-    };
+  body: {
+    userId: string;
+    productId: string;
+  };
 }
 
-export const deleteTrackedProduct = async (req: DeleteTrackedProductRequest, res: Response) => {
-    const { userId, productId } = req.body;
+export const deleteTrackedProduct = async (
+  req: DeleteTrackedProductRequest,
+  res: Response
+) => {
+  const { userId, productId } = req.body;
 
-    try {
-        const user = await User.findById(userId);
-        if (!user) {
-            res.status(404).json({ message: 'User not found' });
-            return
-
-        }
-
-        user.bestBuyProducts = user.bestBuyProducts.filter(
-            (id: mongoose.Types.ObjectId) => id.toString() !== productId
-        );
-
-        await user.save();
-        res.status(200).json({ message: 'Product deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error });
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
     }
+
+    user.bestBuyProducts = user.bestBuyProducts.filter(
+      (id: mongoose.Types.ObjectId) => id.toString() !== productId
+    );
+
+    await user.save();
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
 };
